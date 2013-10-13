8d6408f1a6606a2b4c433fc8dcf442950caff7cd
AronHuang <aronexplorer@gmail.com>
2013-10-13 15:31:24 -0700

    Initial Commit

---
+++ LiveRiotTwitterTest/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  LiveRiotTwitterTest
+//
+//  Created by apple on 10/13/13.
+//  Copyright (c) 2013 HaoyuHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ LiveRiotTwitterTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LiveRiotTwitterTest
+//
+//  Created by apple on 10/13/13.
+//  Copyright (c) 2013 HaoyuHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
